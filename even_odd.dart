0628515fd4a55f9c619a305f25a848e2bebf58de
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-12-18 20:41:55 +0200

    even_odd.dart
    categorize each number then add the even numbers to a list and odd numbers to a list.

--- even_odd.dart
+++ even_odd.dart
@@ -1,29 +1,24 @@
 void main() {
-  List<int> numbers = [
-    4, 3, 6, 73, 357, 889, 345, 332, 65, 89, 442, 978, 10, 14, 13, 26, 27]; //list of inger numbers
-  
+  List<int> numbers = [4,3,6,15,10,14,13,5,26,27]; //list of inger numbers
   //create two empty lists to store even and odd numbers
-   List<int> evenNumbers = [];  //to store even numbers
+  List<int> evenNumbers = [];  //to store even numbers
   List<int> oddNumbers = [];  //to store odd numbers
-
-  //for loop for each number in the list
-  for (int number in numbers) { 
-    
-     //switch case using modulus operator to check if number is even or odd
+  for (int number in numbers) {
+    //for loop for each number in the list
     switch (number % 2) {
-      //if the result is 0 then print number is even  
-      case 0: 
-        evenNumbers.add(number);  //add the even number to the list
-      break; // break the loop to move to the next number
-        //if the result is 1 then print number is odd
-      case 1: 
-        oddNumbers.add(number);  //add the odd number to the list
-      break;
-      //if the result is not 0 or 1 then print unexpected value
-      default:
+      //switch case using modulus operator to check if number is even or odd
+      case 0: //if the result is 0 then print number is even
+        evenNumbers.add(number);  //add the number to the list
+        print('$number is even');  
+        break; // break the loop to move to the next number
+      case 1: //if the result is 1 then print number is odd
+        oddNumbers.add(number);  //add the number to the list
+        print('$number is odd');
+        break;
+      default: //if the result is not 0 or 1 then print unexpected value
         print('Unexpected value');
     };
   };
-  print('Even numbers: $evenNumbers');   //print the list after adding the even numbers to it
-  print('Odd numbers: $oddNumbers');    //print the list after adding the odd numbers to it
+  print('Even Numbers: $evenNumbers');   //print the list after adding the even numbers to it
+  print('Odd Numbers: $oddNumbers');    //print the list after adding the odd numbers to it
 }
